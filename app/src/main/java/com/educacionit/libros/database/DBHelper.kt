a3e660f37ba4c0b00c86cbe176ea0e7c9b871d6f
Jonathan Aranguri <arangurim@gmail.com>
2024-03-13 15:02:27 -0300

    feat: Step 3 - Create tables in onCreate method of DBHelper

--- app/src/main/java/com/educacionit/libros/database/DBHelper.kt
+++ app/src/main/java/com/educacionit/libros/database/DBHelper.kt
@@ -2,20 +2,27 @@ package com.educacionit.libros.database
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import com.educacionit.libros.model.Libro
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper
 import com.j256.ormlite.support.ConnectionSource
+import com.j256.ormlite.table.TableUtils
+import java.sql.SQLException
 
 class DBHelper(context: Context) : OrmLiteSqliteOpenHelper(context, DB_NAME, null, DB_VERSION) {
 
     override fun onCreate(database: SQLiteDatabase?, connectionSource: ConnectionSource?) {
-        TODO("Not yet implemented")
+        try {
+            TableUtils.createTable(connectionSource, Libro::class.java)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
     }
 
     override fun onUpgrade(database: SQLiteDatabase?, connectionSource: ConnectionSource?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
     }
 
-    companion object{
+    companion object {
         const val DB_NAME = "books"
         const val DB_VERSION = 1
     }
